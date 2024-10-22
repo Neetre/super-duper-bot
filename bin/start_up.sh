7b9c4a96963c80c0f524283d0eabc10504def6ce
19746 <mattiabraga2006@gmail.com>
2024-10-22 22:23:36 +0200

    Modified sh

--- bin/start_up.sh
+++ bin/start_up.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 venv_name="/home/mattia/Documents/Git/super-duper-bot/.venv"
 python_program="/home/mattia/Documents/Git/super-duper-bot/bin/main.py"
 requirements_file="/home/mattia/Documents/Git/super-duper-bot/requirements.txt"
 
-if ! [[ -d $venv_name]]; then
-	python3 -m venv $venv_name
+if ! [[ -d $venv_name ]]; then
+    python3 -m venv $venv_name
 fi
 
 source $venv_name/bin/activate
 
-pip3 install -r $requirements_file
+pip install -r $requirements_file
 
 python3 $python_program
